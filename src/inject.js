dca35b9c4e1d8feb10a94ae0e9ab0dbb32d45250
Lucas <onitrog0d@gmail.com>
2021-09-18 16:23:09 -0300

    add localization and small tweaks

--- src/inject.js
+++ src/inject.js
@@ -1,15 +1,21 @@
 // I swear. I tried every single way to listen to when local storage is changed in the same window but it doesn't work. This is my last idea, I tried.
+
+// Set these to a REALLY (I mean it) far timestamp so it doesn't annoy us with popups
+localStorage.setItem('registration-toplayer/expires', 2147483647000);
+localStorage.setItem('offer-modal/timestamp', 2147483647000);
+localStorage.setItem('welcome-payment-form/timestamp', 2147483647000);
+
+// Loop through every key
 Object.keys(localStorage).forEach((key) => {
   // Remove every entry that starts with "flexible-tunnel", that's part of the ads
   if (key.startsWith('flexible-funnel')) {
     localStorage.removeItem(key);
   }
-  // Set these to a REALLY (I mean it) far timestamp so it doesn't annoy us with popups
-  if (key === 'registration-toplayer/expires' || key === 'offer-modal/timestamp' || key === 'welcome-payment-form/timestamp') {
-    localStorage.setItem(key, 2147483647000);
-  }
 });
 
 // Remove annoying banner
-const banner = document.getElementsByClassName('js-react-bottom-banner')[0];
-if (banner) banner.parentNode.removeChild(banner);
+const pathName = window.location.pathname;
+if (pathName.startsWith('/tarefa') || pathName.startsWith('/question')) {
+  const banner = document.getElementsByClassName('js-react-bottom-banner')[0];
+  if (banner) banner.parentNode.removeChild(banner);
+}
